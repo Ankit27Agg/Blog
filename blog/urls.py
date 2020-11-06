ce6dfe5b5ea2162d4b179a271e322f31c098ee8b
Ankit27Agg <worltresting@gmail.com>
2020-11-06 15:03:59 +0530

    File Updated

--- blog/urls.py
+++ blog/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('homeApp.urls')),
-    path('blog', include('myBlogApp.urls'))
+    path('blog/', include('myBlogApp.urls'))
 ]
--- homeApp/views.py
+++ homeApp/views.py
@@ -1,12 +1,12 @@
-from django.shortcuts import render,HttpResponse
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 
 def home(request):
-  return HttpResponse('This is HOME page of our app.')
+  return render(request, 'homeApp/home.html')
     
 def contact(request):
-  return HttpResponse('This is Contact page of our app.')
+  return render(request, 'homeApp/contact.html')
 
 def about(request):
-  return HttpResponse('This is About page of our app.')
+  return render(request, 'homeApp/about.html')

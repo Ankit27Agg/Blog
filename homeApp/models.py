8fd1b0c55d55c8b230ea624bee3b2477ab2b0575
Ankit27Agg <worltresting@gmail.com>
2020-11-09 15:32:57 +0530

    files updated

--- homeApp/models.py
+++ homeApp/models.py
@@ -1,5 +1,9 @@
 from django.db import models
 
 # Create your models here.
-class Contact(models.Models):
+class Contact(models.Model):
   sno = models.AutoField(primary_key=True)
+  name = models.CharField(max_length=255)
+  email = models.CharField(max_length=255)
+  phone = models.CharField(max_length=255)
+  name = models.TextField()
